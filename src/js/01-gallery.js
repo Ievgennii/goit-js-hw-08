cd2c3dd649a0e794d2a3cf6a1d244b0155e2d2e5
Ievgennii <simonovievgenii@gmail.com>
2023-03-01 00:27:57 +0200

    image gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,32 +1,32 @@
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
 
-const divGaleryEl = document.querySelector('.gallery');
+const ulGaleryEl = document.querySelector('.gallery');
 
 // console.log(divGaleryEl);
 
 const addImagesGalery = createImagesGalery(galleryItems);
-divGaleryEl.innerHTML = addImagesGalery;
+ulGaleryEl.innerHTML = addImagesGalery;
 
-divGaleryEl.addEventListener('click', onClickImage);
-
+ulGaleryEl.addEventListener('click', onClickImage);
 
 function createImagesGalery(items) {
   return items
     .map(
       ({ preview, original, description }) =>
-        `<div class="gallery__item">
-        <a class="gallery__link" href="${original}">
+        `<a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
+          src="${preview}"          
+          alt="" title=${description}""
         />
-      </a>
-    </div>`
+      </a>`
     )
     .join('');
 }
@@ -40,15 +40,6 @@ function onClickImage(evt) {
     return;
   }
 
-  const instance = basicLightbox.create(`
-    <img src="${evt.target.dataset.source}" width="800" height="600">
-`);
-// console.log(instance);
-  instance.show();
-
-  window.addEventListener('keydown', (evt) => {if (evt.code === "Escape") {
-    
-    instance.close();
-  }});
-  
+  const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.open();  
 }
