00beafaf9266a6cc2a8ddcf8f794de424020d770
Ievgennii <simonovievgenii@gmail.com>
2023-03-04 00:32:16 +0200

    pre_video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Vimeo from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+console.log(iframe);
+
+const player = new Vimeo(iframe);
+console.log(player);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', function (iframe) {
+  console.log(JSON.stringify(iframe));
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(iframe));
+});
+
+const videoplayerCurrentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+console.log(videoplayerCurrentTime);
+console.log(videoplayerCurrentTime.seconds);
+
+player
+  .setCurrentTime(videoplayerCurrentTime.seconds);
+ 
